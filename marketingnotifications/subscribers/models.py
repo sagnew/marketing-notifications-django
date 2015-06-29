3a68a0dedb24feb1aacd121cdda067493e99cd43
Sam Agnew <sagnew@eden.rutgers.edu>
2015-06-29 03:31:39 -0400

    Implement subscribe and unsubscribe logic

--- marketingnotifications/subscribers/models.py
+++ marketingnotifications/subscribers/models.py
@@ -11,3 +11,22 @@ class Subscriber(models.Model):
 
     def __str__(self):
         return '{0} - {1}'.format(self.phone_number, self.subscribed)
+
+    def handle_message(self, message_body):
+        # Conditional logic to do different things based on the command from
+        # the user
+        if message_body == 'subscribe' or message_body == 'unsub':
+            # If the user has elected to subscribe for messages, flip the bit
+            # and indicate that they have done so.
+            self.subscribed = message_body == 'subscribe'
+            self.save()
+
+            # Otherwise, our subscription has been updated
+            response_message = 'You are now subscribed for updates.'
+            if not self.subscribed:
+                response_message = 'You have unsubscribed. Text "subscribe"' \
+                                   ' to start receiving updates again.'
+            return response_message
+        else:
+            return 'Sorry, we didn\'t understand that. available commands are' \
+                   ': subscribe or unsubscribe'
--- marketingnotifications/subscribers/views.py
+++ marketingnotifications/subscribers/views.py
@@ -7,20 +7,20 @@ from twilio import twiml
 
 @csrf_exempt
 def message(request):
+    response = twiml.Response()
     phone_number = request.POST['From']
+    message_body = request.POST['Body']
 
     try:
+        # If the Subscriber exists, return an appropriate response to this sms.
         sub = Subscriber.objects.get(phone_number=phone_number)
-        return handle_message(sub)
+        reply_message = sub.handle_message(message_body.lower())
+        response.message(reply_message)
     except Subscriber.DoesNotExist:
         # Create a subscriber with this phone number if one does not exist.
         new_subscriber = Subscriber(phone_number=phone_number)
         new_subscriber.save()
-        response = twiml.Response()
         response.message('Thanks for contacting us! Text "subscribe" to '
                          'receive updates via text message.')
-        return HttpResponse(response.toxml(), content_type='text/xml')
 
-
-def handle_message(sub):
-    return HttpResponse()
+    return HttpResponse(response.toxml(), content_type='text/xml')
